cd5b06f8e6095e715fe7c90e3d458ae8101354b8
KcRaymond <kaseyleigh1978@gmail.com>
2021-01-22 18:45:30 -0700

    added JawsDB code to server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Mysqlroot132",
+    password: "Dsmh2020!",
     database: "travel_db",
   });
 }
